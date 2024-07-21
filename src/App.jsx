cf14803bb9ccd09b25996d40b8fd32c099c62b01
gmells <victormells30@gmail.com>
2024-07-21 12:47:13 +0100

    add button component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ const App = () => {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button>Something</Button>
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
       </div>
       <ButtonGradient />
     </>
