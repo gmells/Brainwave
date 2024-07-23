fb6fb909df4be381e8a46bc4803376cca4d5a6f5
gmells <victormells30@gmail.com>
2024-07-23 22:54:59 +0100

    Create braiwave logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
+import { brainwave } from "../assets";
+
 const Header = () => {
   return (
     <div
       className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm 
     lg:bg-n-8/90 lg:backdrop:blur-sm border-b border-n-6 "
     >
-      Header
+      <div
+        className="flex items-center px-5 lg:px-7.5 
+      xl:px-10 max-lg:py-4"
+      >
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwave} width={190} height={40} alt="brainwave" />
+        </a>
+      </div>
     </div>
   );
 };
