37af4874bd945c0fc47ad7339656433a88573c21
gmells <victormells30@gmail.com>
2024-07-24 21:55:01 +0100

    create constants folder containing js file and update header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,18 @@ const Header = () => {
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="brainwave" />
         </a>
+
+        <nav
+          className="hidden fixed top-[5rem] left-0 right-0 bottom-0 
+        bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent "
+        >
+          <div
+            className="relative z-2 flex flex-col items-center 
+          justify-center lg:flex-row"
+          >
+            1 2 3
+          </div>
+        </nav>
       </div>
     </div>
   );
