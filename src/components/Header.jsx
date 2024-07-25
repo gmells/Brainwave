8bb8962b53221f0a336dabe599672ecd308c1d33
gmells <victormells30@gmail.com>
2024-07-25 16:17:45 +0100

    complete header in larger screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import { brainwave } from "../assets";
+import { useLocation } from "react-router-dom";
+import { navigation } from "../constants";
+import Button from "./Button";
 
 const Header = () => {
+  const pathname = useLocation();
+
   return (
     <div
-      className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm 
+      className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm 
     lg:bg-n-8/90 lg:backdrop:blur-sm border-b border-n-6 "
     >
       <div
@@ -22,12 +27,43 @@ const Header = () => {
             className="relative z-2 flex flex-col items-center 
           justify-center lg:flex-row"
           >
-            1 2 3
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                className={`block 
+              relative font-code text-2xl uppercase text-n-1
+              transition-colors hover:text-color-1 ${
+                item.onlyMobile ? "lg:hidden" : ""
+              } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold
+              ${
+                item.url === pathname.hash
+                  ? "z-2 lg:text-n-1"
+                  : "lg:text-n-1/50"
+              } lg:leading-5 lg:hover:text-n-1
+              xl:px-12`}
+              >
+                {item.title}
+              </a>
+            ))}
           </div>
         </nav>
+
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50
+        transition-colors hover:text-n-1 lg:block"
+        >
+          New Account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign in
+        </Button>
       </div>
     </div>
   );
 };
 
 export default Header;
+
+//extra space innit
