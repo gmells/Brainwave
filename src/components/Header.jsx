297c378036b7d14f11b2bec623d3746093c6e781
gmells <victormells30@gmail.com>
2024-07-28 15:32:24 +0100

    run header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,18 @@ import { brainwave } from "../assets";
 import { useLocation } from "react-router-dom";
 import { navigation } from "../constants";
 import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
   const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
 
   return (
     <div
-      className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm 
-    lg:bg-n-8/90 lg:backdrop:blur-sm border-b border-n-6 "
+      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm 
+    lg:bg-n-8/90 lg:backdrop:blur-sm border-b border-n-6 ${open} `}
     >
       <div
         className="flex items-center px-5 lg:px-7.5 
