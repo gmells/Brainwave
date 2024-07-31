d22b95ac68d36569c7d55345e3d2287696a3b95a
gmells <victormells30@gmail.com>
2024-07-31 17:35:27 +0100

    fix button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -16,7 +16,7 @@ const Button = ({ className, href, onClick, children, px, white }) => {
   );
 
   const renderLink = () => (
-    <a className={classes}>
+    <a href={href} className={classes}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </a>
