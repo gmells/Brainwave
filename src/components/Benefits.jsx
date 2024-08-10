f1c016e71d23f68fafd57e4bdef16858a585e020
gmells <victormells30@gmail.com>
2024-08-10 19:42:50 +0100

    Complete benefit component

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,6 +3,7 @@ import Heading from "./Heading";
 import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -10,7 +11,7 @@ const Benefits = () => {
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
-          title="Chat Smarter Not Harder with Brainwave"
+          title="Chat Smarter, Not Harder with Brainwave"
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
@@ -21,7 +22,7 @@ const Benefits = () => {
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h2 mb-5">{item.title}</h5>
+                <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
                   <img
@@ -30,7 +31,7 @@ const Benefits = () => {
                     height={48}
                     alt={item.title}
                   />
-                  <p className="ml-auto font-code text-xl font-bold text-n-1 uppercase tracking-wider">
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
                     Explore more
                   </p>
                   <Arrow />
@@ -39,7 +40,24 @@ const Benefits = () => {
 
               {item.light && <GradientLight />}
 
-              <div className="absolute inset-0.5 bg-n-8" style={{ clipPath: }}></div>
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+
+              <ClipPath />
             </div>
           ))}
         </div>
